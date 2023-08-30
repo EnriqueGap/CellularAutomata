c28f05b8a6dadcbdfc82ca57f39ae290878aac6b
Enrique Galicia <enrique.galicia.pineda@gmail.com>
2023-08-30 01:54:39 -0600

    OOP changed, class for ECA

--- src/eca.py
+++ src/eca.py
@@ -1,58 +1,74 @@
 from random import randint
-from matplotlib.pyplot import matshow, show, savefig
 # CellularAutomata 1D
-BAND_LENGTH = 150
-MAX_ITERS = 500
-rule_err = False
-band_err = False
 
-def ruleToBinary(rule:int) -> list[int]:
-	if (rule>2**8 - 1)|(rule<0):
-		binary_rule = "0"*8
-		rule_err = True
-	else:
-		binary_rule = bin(rule)[2:]
-		binary_rule = "0"*(8-len(binary_rule)) + binary_rule
-	return [int(c) for c in binary_rule]
+class ECA(object):
+	def __init__(self, band_length:int, max_iters:int):
+		self.band_length = band_length
+		self.max_iters = max_iters
+		self.rule_err = False
+		self.band_err = False
+		self.band = []
+		self.rule = []
+		self.matrix = []
+		
+	def setRule(self, rule:int) -> list[int]:
+		if (rule>2**8 - 1)|(rule<0):
+			binary_rule = "0"*8
+			self.rule_err = True
+		else:
+			binary_rule = bin(rule)[2:]
+			binary_rule = "0"*(8-len(binary_rule)) + binary_rule
+			self.rule = [int(c) for c in binary_rule]
+		return [int(c) for c in binary_rule]
 
-def initializeBand(band_init:int = None, random_init=True) -> list[int]:
-	if band_init:
-		random_init=False
-	if random_init:
-		band = randint(0,2**BAND_LENGTH - 1)
-	elif (band_init >2**BAND_LENGTH - 1)|(band_init<0):
-		band = 0
-		band_err = True
-	else:
-		band = band_init
+	def setBand(self, band_init:int = None, random_init=True) -> list[int]:
+		self.band = []
+		if band_init:
+			random_init=False
+		if random_init:
+			band = randint(0,2**self.band_length - 1)
+		elif (band_init >2**self.band_length - 1)|(band_init<0):
+			band = 0
+			self.band_err = True
+		else:
+			band = band_init
+	
+		band = bin(band)[2:]
+		band = "0"*(self.band_length - len(band)) + band
+		self.band = [int(c) for c in band]
+		return [int(c) for c in band]
 
-	band = bin(band)[2:]
-	band = "0"*(BAND_LENGTH - len(band)) + band
-	return [int(c) for c in band]
+	def iterateBand(self) -> list[int]:
+		aux_band = [0]*self.band_length
+		for i in range(1, self.band_length-1):
+			if self.band[i-1] == 1:
+				if self.band[i] == 1:
+					if self.band[i+1] == 1:
+						aux_band[i] = self.rule[0]
+					elif self.band[i+1]==0:
+						aux_band[i] = self.rule[1]
+				elif self.band[i] == 0:
+					if self.band[i+1] == 1:
+						aux_band[i] = self.rule[2]
+					elif self.band[i+1]==0:
+						aux_band[i] = self.rule[3]
+			elif self.band[i-1] == 0:
+				if self.band[i] == 1:
+					if self.band[i+1] == 1:
+						aux_band[i] = self.rule[4]
+					elif self.band[i+1] == 0:
+						aux_band[i] = self.rule[5]
+				elif self.band[i] == 0:
+					if self.band[i+1] == 1:
+						aux_band[i] = self.rule[6]
+					elif self.band[i+1] == 0:
+						aux_band[i] = self.rule[7]
+		self.band = aux_band
+		return aux_band
 
-def iterateBand(band:list[int], rule:list[int]) -> list[int]:
-	aux_band = [0]*BAND_LENGTH
-	for i in range(1, BAND_LENGTH-1):
-		if band[i-1] == 1:
-			if band[i] == 1:
-				if band[i+1] == 1:
-					aux_band[i] = rule[0]
-				elif band[i+1]==0:
-					aux_band[i] = rule[1]
-			elif band[i] == 0:
-				if band[i+1] == 1:
-					aux_band[i] = rule[2]
-				elif band[i+1]==0:
-					aux_band[i] = rule[3]
-		elif band[i-1] == 0:
-			if band[i] == 1:
-				if band[i+1] == 1:
-					aux_band[i] = rule[4]
-				elif band[i+1] == 0:
-					aux_band[i] = rule[5]
-			elif band[i] == 0:
-				if band[i+1] == 1:
-					aux_band[i] = rule[6]
-				elif band[i+1] == 0:
-					aux_band[i] = rule[7]
-	return aux_band
+	def evolveBand(self) -> None:
+		self.matrix = []
+		self.matrix.append(self.band)
+		for i in range(0,self.max_iters):
+			self.iterateBand()
+			self.matrix.append(self.band)
