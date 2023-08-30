2a8b6d544c08f6433b3b6421963554e86ae89010
Enrique Galicia <enrique.galicia.pineda@gmail.com>
2023-08-30 01:54:56 -0600

    Streamlit app running

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import streamlit as st
 import time
+import src.eca as eca
+import matplotlib.pyplot as plt
 
 icon = Image.open("./media/page_icon.png")
 st.set_page_config(page_title="Cellular Automata",
@@ -23,79 +25,44 @@ st.sidebar.write('2023')
 st.sidebar.write('How to reach me')
 st.sidebar.write('[Enrique Galicia](https://enriquegap.github.io)')
 
-from random import randint
-from matplotlib.pyplot import matshow, show, savefig
-# CellularAutomata 1D
-BAND_LENGTH = 150
-MAX_ITERS = 500
-rule_err = False
-band_err = False
+if nav=='Elementary Cellular Automaton':
+    st.subheader('Elementary Cellular Automaton')
+    st.markdown('___')
+    rule = st.selectbox('Select Rules',list(range(0,2**8)))
+    iterations = st.text_input('Number of iterations')
+    try:
+    	iterations = int(iterations)
+    except:
+    	st.write("Please write a number for iterations")
+    size_band = st.text_input('Size of the band')
+    try:
+    	size_band = int(size_band)
+    except:
+    	st.write("Please write a number to determine the size of the band")
 
-def ruleToBinary(rule:int) -> list[int]:
-	if (rule>2**8 - 1)|(rule<0):
-		binary_rule = "0"*8
-		rule_err = True
-	else:
-		binary_rule = bin(rule)[2:]
-		binary_rule = "0"*(8-len(binary_rule)) + binary_rule
-	return [int(c) for c in binary_rule]
+    if not st.toggle('Random initialization', help='Switch to specify initialization', value=True):
+    	initialization = st.text_input("Please give an initialization")
+    	try:
+    		initialization = int(initialization)
+    		if (initialization<0)|(initialization>2**size_band-1):
+    			st.write("Please enter a valid initialization")
+    		else:
+    			automata = eca.ECA(size_band, iterations)
+    			automata.setRule(rule)
+    			automata.setBand(initialization)
+    	except:
+    		st.write("Please write a number for initialization")
 
-def initializeBand(band_init:int = None, random_init=True) -> list[int]:
-	if band_init:
-		random_init=False
-	if random_init:
-		band = randint(0,2**BAND_LENGTH - 1)
-	elif (band_init >2**BAND_LENGTH - 1)|(band_init<0):
-		band = 0
-		band_err = True
-	else:
-		band = band_init
-
-	band = bin(band)[2:]
-	band = "0"*(BAND_LENGTH - len(band)) + band
-	return [int(c) for c in band]
-
-def iterateBand(band:list[int], rule:list[int]) -> list[int]:
-	aux_band = [0]*BAND_LENGTH
-	for i in range(1, BAND_LENGTH-1):
-		if band[i-1] == 1:
-			if band[i] == 1:
-				if band[i+1] == 1:
-					aux_band[i] = rule[0]
-				elif band[i+1]==0:
-					aux_band[i] = rule[1]
-			elif band[i] == 0:
-				if band[i+1] == 1:
-					aux_band[i] = rule[2]
-				elif band[i+1]==0:
-					aux_band[i] = rule[3]
-		elif band[i-1] == 0:
-			if band[i] == 1:
-				if band[i+1] == 1:
-					aux_band[i] = rule[4]
-				elif band[i+1] == 0:
-					aux_band[i] = rule[5]
-			elif band[i] == 0:
-				if band[i+1] == 1:
-					aux_band[i] = rule[6]
-				elif band[i+1] == 0:
-					aux_band[i] = rule[7]
-	return aux_band
-
-
-def getPlot(band_number:int, rule_number:int) -> None:
-	matrix = []
-	rule = ruleToBinary(rule_number)
-	print(rule)
-	band = initializeBand(band_number)
-	print(band)
-	matrix.append(band)
-	band_itered = iterateBand(band, rule)
-	for i in range(0,MAX_ITERS):
-		band_itered = iterateBand(band_itered, rule)
-		matrix.append(band_itered)
-	matshow(matrix)
-	savefig("sierpinski.png")
-	show()
-	
-getPlot(2**25, 90)
+    else:
+    	if (size_band!="")&(iterations!=""):
+	    	automata = eca.ECA(size_band, iterations)
+    		automata.setRule(rule)
+    		automata.setBand()   
+    		 	
+    if st.button('Go!'):
+    	automata.evolveBand()
+    	fig, ax = plt.subplots()  ## <- Create matplotlib Figure & Axes
+    	ax.matshow(automata.matrix, cmap="Greys")
+    	ax.get_xaxis().set_visible(False)
+    	ax.get_yaxis().set_visible(False)
+    	st.pyplot(fig)  ## <- Tell Streamlit to show that figure
